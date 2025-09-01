587370886bad730844eb7e375a78e9053f862ac2
Gabriel Jabes <gabriel.jabesldl@gmail.com>
2025-09-01 16:37:02 -0300

    ainda falta criar a funçao de exibir todos os produtos e buscar por nome

---
+++ structs9.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*9. Considere uma loja de eletrônicos que vende três tipos de produtos: smartphones, notebooks
+e televisores. Cada produto possui as seguintes informações: nome, marca, modelo e preço.
+Além disso, cada produto possui informações específicas, descritas a seguir:
+
+• Smartphone: sistema operacional (Android ou iOS) e capacidade de armazenamento (em GB).
+• Notebook: tamanho da tela (em polegadas) e quantidade de memória RAM (em GB).
+• Televisor: tamanho da tela (em polegadas) e resolução (HD, Full HD, 4K).
+
+a) Crie uma estrutura de dados chamada "Produto" que armazene todas as informações
+descritas acima. Utilize as estruturas "enum" e "union" quando necessário. 
+
+b Crie uma função que receba como parâmetro um produto e imprima na tela todas as suas informações,
+incluindo as informações específicas de cada tipo de produto. 
+
+c) Crie um programa que
+permita ao usuário cadastrar produtos na loja, imprimir na tela os produtos cadastrados e
+buscar um produto pelo seu nome.
+*/
+typedef enum{
+    SMARTPHONE, NOTEBOOK,  TELEVISOR,
+} tipo_produto;
+
+typedef struct{
+    char nome[50], marca[10], modelo[10];
+} info_produto;
+
+typedef union{
+    float tamanho_tela;
+    int memoria_ram, memoria_ssd;
+    char resolucao[8], so[8];
+} info_especifica;
+
+typedef struct{
+    tipo_produto tipo;
+    info_produto info;
+    info_especifica info_espec1, info_espec2;
+} produto;
+
+void printProduto(produto p);
+void cadastrarProduto(produto *p, int *n);
+
+
+int main(){
+    int qtd_produtos = 0;
+    produto *produtos = NULL;
+    char resposta = 's';
+
+    while(resposta == 'S' || resposta == 's'){
+    cadastrarProduto(produtos, &qtd_produtos);
+    printf("\ndeseja cadastrar mais produtos?\n");
+    scanf(" %c", &resposta);
+    } 
+
+    printf("cabou");
+
+ free(produtos);
+    return 0;
+}
+
+
+
+void printProduto(produto p){
+    
+    printf("\tMarca: %s\tNome: %s\tModelo: %s\t", p.info.marca, p.info.nome, p.info.modelo);
+    switch (p.tipo){
+        case SMARTPHONE:
+            printf("Sistema Operacional: %s\tArmazenamento: %d GB\t Categoria: SMARTPHONE\t\n", p.info_espec1.so, p.info_espec2.memoria_ssd);
+            break;
+        case NOTEBOOK:
+            printf("Tamanho da tela: %.2f polegadas\tRAM: %d GB\t Categoria: NOTEBOOK\t\n", p.info_espec1.tamanho_tela, p.info_espec2.memoria_ram);
+            break;
+        case TELEVISOR:
+            printf("Tamanho da tela: %.2f polegadas\tResolucao: %s\t Categoria: TELEVISOR\t\n", p.info_espec1.tamanho_tela, p.info_espec2.resolucao);
+            break;
+        default:
+            printf("Categoria: N/A\t\n");
+            break;
+    }
+}
+
+void cadastrarProduto(produto *p, int *qtd_produtos){
+    char categoria[12];
+    produto *ptr_produto = (produto *)realloc(p, *qtd_produtos + 1 * sizeof(produto));
+    if(ptr_produto == NULL){
+        printf("memoria nao foi alocada corretamente!");
+        free(p);
+    }
+
+    p = ptr_produto;
+
+    printf("digite a categoria do produto: \n");
+    scanf("%s", categoria);
+    if(strcmp(categoria, "smartphone") == 0){
+        p->tipo = SMARTPHONE;
+        } else if(strcmp(categoria, "notebook") == 0){
+            p->tipo = NOTEBOOK;
+            } else if(strcmp(categoria, "televisor") == 0){
+                p->tipo = TELEVISOR;
+                }
+    getchar();
+    printf("digite o nome do produto: \n");
+        fgets(p->info.nome, 50, stdin);
+        p->info.nome[strcspn(p->info.nome, "\n")] = '\0'; // tira o \n da string (tava atrapalhando no output)
+    printf("digite a marca do produto: \n");
+        fgets(p->info.marca, 10, stdin);
+        p->info.marca[strcspn(p->info.marca, "\n")] = '\0'; 
+    printf("digite o modelo do produto: \n");
+        fgets(p->info.modelo, 10, stdin);
+        p->info.modelo[strcspn(p->info.modelo, "\n")] = '\0';
+
+    switch(p->tipo){
+        case SMARTPHONE:
+            printf("digite o sistema operacional do smartphone: \n");
+            fgets(p->info_espec1.so, 8, stdin);
+            printf("digite o armazenamento do smartphone: \n");
+            scanf("%d", &p->info_espec2.memoria_ssd);
+            break;
+        case NOTEBOOK:
+            printf("digite o tamanho da tela do notebook: \n");
+            scanf("%f", &p->info_espec1.tamanho_tela);
+            printf("digite a quantidade de RAM de do notebook: \n");
+            scanf("%d", &p->info_espec2.memoria_ram);
+            break;
+        case TELEVISOR:
+            printf("digite o tamanho da tela do televisor: \n");
+            scanf("%f", &p->info_espec1.tamanho_tela);
+            printf("digite a resolucao (HD, Full HD, 4K) do televisor:  \n");
+            fgets(p->info_espec2.resolucao, 8, stdin);
+            break;
+        default:
+            break;
+    }
+
+     qtd_produtos++;
+
+    printf("=== EXIBINDO PRODUTO CADASTRADO ===\n");
+    printProduto(*p);
+    
+}
